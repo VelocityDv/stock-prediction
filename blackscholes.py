dff89194b860aabc52318f1355f36fd130639c25
EdenZhangGithub <eden@Archlinux.localdomain>
2022-02-22 17:45:02 +1300

    initialize variables

---
+++ blackscholes.py
@@ -0,0 +1,15 @@
+class BlackScholes():
+    def __init__(self):
+        # call = true / put = false
+        self.call = True
+        # spot price of asset
+        self.St = 0
+        # strike price
+        self.k = 0 
+        # risk free rate
+        self.r = 0
+        # time to maturity
+        self.t = 0
+        # volatility of asset
+        self.sigma = 0
+        
